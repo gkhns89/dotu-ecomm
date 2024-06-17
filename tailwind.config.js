e5ec1ef0ea543a4f32c269fd092438da516554d8
Gökhan <gokhansisman@outlook.com>
2024-06-18 00:08:13 +0300

    Tailwind config, font and base layout settings are updated. Fixes #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+
     extend: {},
+    fontFamily: {
+      'sans': ['Montserrat', 'system-ui'],
+      'serif': ['ui-serif', 'Montserrat'],
+      'mono': ['ui-monospace', 'Montserrat'],
+    }
   },
   plugins: [],
 }
