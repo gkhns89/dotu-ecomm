556e85300c9b53343966be9001e36610f23cf7bb
Gökhan <gokhansisman@outlook.com>
2024-06-20 00:45:11 +0300

    Main colors setted, layout components prepared and started to create header section. Fixes #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import shopLogo from './assets/react.svg'
-import './App.css'
-
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartShopping,
+  faUser,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 function App() {
-
   return (
-    <>
+   
       <div>
-        <a href="https://react.dev" target="_blank">FONT TEST
-          <img src={shopLogo} className="logo" alt="React logo" />
-        </a>
+        <header className="min-w-[414px] mx-8 my-6 flex">
+          <div id="header-top">
+            <div
+              id="logo"
+              className="min-w-[153px] font-bold leading-8 tracking-[0.1px] text-txt-b"
+            >
+              DotuTasarım
+            </div>
+
+            <div id="header-buttons">
+              <FontAwesomeIcon icon={faUser} bounce />
+              <FontAwesomeIcon icon={faCartShopping} bounce />
+              <FontAwesomeIcon icon={faBars} bounce />
+            </div>
+          </div>
+
+          <div id="header-content"></div>
+        </header>
       </div>
-      
-    </>
-  )
+   
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      c1: "#033465",
+      c2: "#81e8dc",
+      c3: "#4acace",
+      c4: "#f1ff5e",
+      c5: "#5df4bf",
+
+      txt: {
+        b: "#121521",
+        g: "#737373",
+        w: "#f9ffff",
+      },
+    },
 
     extend: {},
+
     fontFamily: {
-      'sans': ['Montserrat'],
-      'serif': ['Montserrat'],
-      'mono': ['Montserrat'],
-    }
+      sans: ["Montserrat"],
+      serif: ["Montserrat"],
+      mono: ["Montserrat"],
+    },
   },
   plugins: [],
-}
-
+};
