75d538561c187f387b478a47d6335e4408f7f4b6
Gökhan <gokhansisman@outlook.com>
2024-06-20 23:43:41 +0300

    Header styling fixes, search components added, aria-label usage implememted Fixes #2

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,36 @@ import {
   faCartShopping,
   faUser,
   faBars,
+  faSearch
 } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
    
       <div>
-        <header className="min-w-[414px] mx-8 my-6 flex">
-          <div id="header-top">
+        <header aria-label="header" className="min-w-[414px] mx-8 my-6">
+          <div aria-label="header-top" className="flex justify-between">
             <div
-              id="logo"
-              className="min-w-[153px] font-bold leading-8 tracking-[0.1px] text-txt-b"
+              aria-label="logo"
+              className="flex items-center min-w-[153px] font-bold leading-8 tracking-[0.1px] text-txt-b"
             >
               DotuTasarım
             </div>
-
-            <div id="header-buttons">
-              <FontAwesomeIcon icon={faUser} bounce />
-              <FontAwesomeIcon icon={faCartShopping} bounce />
-              <FontAwesomeIcon icon={faBars} bounce />
+            <div aria-label="header-content" className="sm:flex items-center hidden">
+              <input type="text" placeholder="Search" className="border-2 border-gray-400 p-2" />
+              <select className="border-2 border-gray-400">
+                <option>Black Friday</option>
+                <option>Outlet</option>
+              </select>
+              <button><FontAwesomeIcon icon={faSearch} /></button>
+            </div>
+            <div aria-label="header-buttons" className="flex gap-1 items-center">
+              <FontAwesomeIcon icon={faUser} />
+              <FontAwesomeIcon icon={faCartShopping} />
+              <FontAwesomeIcon icon={faBars} />
             </div>
           </div>
 
-          <div id="header-content"></div>
+          
         </header>
       </div>
    
