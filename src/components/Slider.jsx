d29baa25fe832c8d03a69bb2346c07e29930d199
Gökhan <gokhansisman@outlook.com>
2024-06-26 14:42:15 +0300

    Adding images pre install swiper slider editor's pick area prepared Fixes #2

---
+++ src/components/Slider.jsx
@@ -0,0 +1,21 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+
+import './styles.css';
+
+// import required modules
+import {Navigation, Scrollbar } from 'swiper/modules';
+
+export default function App() {
+  return (
+    <>
+
+    </>
+  );
+}
